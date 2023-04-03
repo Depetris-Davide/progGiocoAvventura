692f18f28e2d640a784cd01a96c79305a2f4f945
davide.depetris <davide.depetris@InfoTele14.maxwell.local>
2023-04-03 11:38:15 +0200

    NEMICO CHE FUNZIONA CIRCA

--- src/proggiocoavventura/Nemico.java
+++ src/proggiocoavventura/Nemico.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proggiocoavventura;
 
 import java.util.ArrayList;
 import javax.swing.JLabel;
 
-/**
- *
- * @author gigga
- */
 public class Nemico extends JLabel {
     
     int livello;
@@ -18,14 +10,12 @@ public class Nemico extends JLabel {
     int size;
     String domanda;
     ArrayList<String> risposte;
-    String terreno;
     
     //ImageIcon player;
 
-    Nemico(String terreno,String domanda,ArrayList<String> risposte,int posX, int posY) {
+    Nemico(String domanda,ArrayList<String> risposte,int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
-        this.terreno = terreno;
         this.domanda = domanda;
         this.risposte = risposte;
         size = 40;

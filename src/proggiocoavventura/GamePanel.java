ff582cf5e904ee6cafdbba780447b0a4a4ba4fdd
Gabriel Ricaldone <gabriel.ricaldone@jcmaxwell.it>
2023-04-11 16:31:11 +0200

    Aggiunta nemico file

--- src/proggiocoavventura/GamePanel.java
+++ src/proggiocoavventura/GamePanel.java
@@ -9,12 +9,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable {
 
     Personaggio papa;
-    Nemico prova;
+    LinkedList<Nemico> nemici;
     Thread gameThread;
     KeyHandler keyH;
     int mondoSizeX, mondoSizeY;
@@ -29,6 +32,7 @@ public class GamePanel extends JPanel implements Runnable {
     JLabel domandaText = new JLabel();
 
     GamePanel() throws IOException {
+        nemici = new LinkedList<Nemico>();
         domandaBG.setBounds(0, 0, 1300, 200);
         domandaText.setBounds(0, 0, 1300, 150);
         risposte.add("4");
@@ -36,7 +40,6 @@ public class GamePanel extends JPanel implements Runnable {
         mondoSizeX = 26;
         mondoSizeY = 14;
         papa = new Personaggio(11, 6);
-        prova = new Nemico("Quanto fa 14-7+2?", risposte, 11, 7);
         keyH = new KeyHandler();
         mondo = new Piattaforma[mondoSizeY][mondoSizeX];
         stat = new JButton("Statistiche");
@@ -62,7 +65,7 @@ public class GamePanel extends JPanel implements Runnable {
         this.setLayout(null);
         this.addKeyListener(keyH);
         this.setFocusable(true);
-        creaMondo("mondo" + mondoY + "-" + mondoX + ".txt");
+        creaMondo(0, 0);
 
         startGameThread();
     }
@@ -100,49 +103,51 @@ public class GamePanel extends JPanel implements Runnable {
         if (papa.posX >= mondoSizeX) {
             mondoX++;
             nuovoMondo = "mondo" + mondoY + "-" + mondoX + ".txt";
-            creaMondo(nuovoMondo);
+            creaMondo(mondoY, mondoX);
             papa.posX = 0;
         } else if (papa.posX < 0) {
             mondoX--;
             nuovoMondo = "mondo" + mondoY + "-" + mondoX + ".txt";
-            creaMondo(nuovoMondo);
+            creaMondo(mondoY, mondoX);
             papa.posX = mondoSizeX - 1;
         }
         if (papa.posY >= mondoSizeY) {
             mondoY++;
             nuovoMondo = "mondo" + mondoY + "-" + mondoX + ".txt";
-            creaMondo(nuovoMondo);
+            creaMondo(mondoY, mondoX);
             papa.posY = 0;
         } else if (papa.posY < 0) {
             mondoY--;
             nuovoMondo = "mondo" + mondoY + "-" + mondoX + ".txt";
-            creaMondo(nuovoMondo);
+            creaMondo(mondoY, mondoX);
             papa.posY = mondoSizeY - 1;
         }
 
         if (mondo[papa.posY][papa.posX].terreno.equals("acqua")) {
             papa.posX = 11;
             papa.posY = 6;
-            creaMondo("mondo0-0.txt");
+            creaMondo(0, 0);
             mondoX = 0;
             mondoY = 0;
         }
 
-        if (papa.posX == prova.posX && papa.posY == prova.posY) {
-            if (keyH.right) {
-                papa.posX -= 1;
+        for (Nemico nemico : nemici) {
+            if (papa.posX == nemico.posX && papa.posY == nemico.posY) {
+                if (keyH.right) {
+                    papa.posX -= 1;
+                }
+                if (keyH.up) {
+                    papa.posY += 1;
+                }
+                if (keyH.left) {
+                    papa.posX += 1;
+                }
+                if (keyH.down) {
+                    papa.posY -= 1;
+                }
+                domanda(nemico);
+                System.out.println("collisione");
             }
-            if (keyH.up) {
-                papa.posY += 1;
-            }
-            if (keyH.left) {
-                papa.posX += 1;
-            }
-            if (keyH.down) {
-                papa.posY -= 1;
-            }
-            domanda();
-            System.out.println("collisione");
         }
 
         if (keyH.up) {
@@ -192,7 +197,9 @@ public class GamePanel extends JPanel implements Runnable {
         }
         g.fillRect((papa.posX * 50) + 5, (papa.posY * 50) + 5, papa.size, papa.size);
         g.setColor(Color.red);
-        g.fillRect((prova.posX * 50) + 5, (prova.posY * 50) + 5, prova.size, prova.size);
+        for (Nemico nemico : nemici) {
+            g.fillRect((nemico.posX * 50) + 5, (nemico.posY * 50) + 5, nemico.size, nemico.size);
+        }
     }
 
     public void creaStatPanel() {
@@ -212,22 +219,87 @@ public class GamePanel extends JPanel implements Runnable {
         livello.setText("Livello: " + papa.livello);
     }
 
-    public void creaMondo(String cambiaMondo) throws FileNotFoundException, IOException {
-        BufferedReader filein = new BufferedReader(new FileReader(cambiaMondo));
-        String s;
-        System.out.println(cambiaMondo);
+    public void creaMondo(int MondoY, int MondoX) throws IOException {
+        boolean errNem = false, errMondo = false;
 
-        for (int i = 0; i < mondoSizeY; i++) {
-            for (int j = 0; j < mondoSizeX; j++) {
-                s = filein.readLine();
-                mondo[i][j] = new Piattaforma(s, i, j);
-            }
+        BufferedReader fileinMondo = null;
+        try {
+            fileinMondo = new BufferedReader(new FileReader("mondo" + MondoY + "-" + MondoX + ".txt"));
+        } catch (FileNotFoundException ex) {
+            errMondo = true;
         }
+        BufferedReader fileinNemico = null;
+        try {
+            fileinNemico = new BufferedReader(new FileReader("nemico" + MondoY + "-" + MondoX + ".txt"));
+        } catch (FileNotFoundException ex) {
+            errNem = true;
+        }
+
+        String s = "";
+        String[] tok;
+        String[] tokRisp;
+
+        String domanda = "";
+        ArrayList<String> risposteTemp = new ArrayList<String>();
+        int posX = 0, posY = 0;
+
+        if (nemici != null) {
+            nemici.clear();
+        }
+        if (!errMondo) {
+            for (int i = 0; i < mondoSizeY; i++) {
+                for (int j = 0; j < mondoSizeX; j++) {
+                    s = fileinMondo.readLine();
+                    mondo[i][j] = new Piattaforma(s, i, j);
+                }
+            }
+            fileinMondo.close();
+        } else {
+            for (int i = 0; i < mondoSizeY; i++) {
+                for (int j = 0; j < mondoSizeX; j++) {
+                    mondo[i][j] = new Piattaforma("acqua", i, j);
+                }
+            }
+
+        }
+        if (!errNem) {
+            do {
+                s = fileinNemico.readLine();
+                if (s != null) {
+                    tok = s.split("\\®");
+                    for (int i = 0; i < 4; i++) {
+                        switch (i) {
+                            case 0:
+                                System.out.println(tok[0]);
+                                domanda = tok[0];
+                                break;
+                            case 1:
+                                System.out.println(tok[1]);
+                                tokRisp = tok[1].split("\\|");
+                                for (String singRisp : tokRisp) {
+                                    risposteTemp.add(singRisp);
+                                }
+                                break;
+                            case 2:
+                                System.out.println(tok[1]);
+                                posX = Integer.parseInt(tok[2]);
+                                break;
+                            case 3:
+                                posY = Integer.parseInt(tok[3]);
+                                break;
+                        }
+                    }
+                    nemici.add(new Nemico(domanda, risposteTemp, posX, posY));
+                }
+            } while (s != null);
+            fileinNemico.close();
+        }
+
     }
 
-    public void domanda() {
+    public void domanda(Nemico nemico) {
         if (papa.livello >= 1 && papa.livello <= 5) {
-            domandaText.setText(prova.domanda);
+            domandaText.setText(nemico.domanda);
         }
         domandaBG.add(domandaText);
         this.add(domandaBG);
